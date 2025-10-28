8379c11b84ac438f42bb32673e33259c81e542c8
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2025-10-28 15:13:28 -0300

    incluindo métodos DonwloadArquivo, DeletarArquivo e Listar; criada class BlobDto

--- Controllers/ArquivosController.cs
+++ Controllers/ArquivosController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
@@ -35,6 +36,51 @@ namespace azureblobstorageapi.Controllers
 
             return Ok(blob.Uri.ToString());
         }
+
+        [HttpGet("Downloadd/{nome}")]
+        public IActionResult DownloadArquivo(string nome)
+        {
+            BlobContainerClient container = new(_connectionString, _containerName);
+            BlobClient blob = container.GetBlobClient(nome);
+
+            if (!blob.Exists())
+                return BadRequest();
+
+            var retorno = blob.DownloadContent();
+            return File(retorno.Value.Content.ToArray(), retorno.Value.Details.ContentType, blob.Name);
+        }
+
+        [HttpDelete("Apagar/{nome}")]
+        public IActionResult DeletarArquivo(string nome)
+        {
+            BlobContainerClient container = new(_connectionString, _containerName);
+            BlobClient blob = container.GetBlobClient(nome);
+
+            blob.DeleteIfExists();
+            return NoContent();
+        }
+
+        [HttpGet("Listar")]
+        public IActionResult Listar()
+        {
+            List<BlobDto> blobsDto = new List<BlobDto>();
+            BlobContainerClient container = new(_connectionString, _containerName);
+
+            foreach (var blob in container.GetBlobs())
+            {
+                blobsDto.Add(new BlobDto
+                {
+                    Nome = blob.Name,
+                    Tipo = blob.Properties.ContentType,
+                    Uri = container.Uri.AbsoluteUri + "/" + blob.Name
+                });
+            }
+
+            return Ok(blobsDto);
+
+        }
+
+
 
 
     }
